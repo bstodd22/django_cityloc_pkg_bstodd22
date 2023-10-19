9c4b3cf6a68faf03f2387cdfa3647963c91ce854
Brayden Todd <braydentodd104@gmail.com>
2023-10-18 18:38:41 -0700

    publish package

--- setup.py
+++ setup.py
@@ -6,4 +6,4 @@ from pathlib import Path
 this_directory = Path(__file__).parent
 long_description = (this_directory / "README.rst").read_text()
 
-setup(long_description=long_description)
+setup(long_description=long_description, long_description_content_type="text/markdown")
